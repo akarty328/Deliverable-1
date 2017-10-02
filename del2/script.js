54413a050864581d3dee32eb6586774f572da396
Adam Karty <akarty328@gmail.com>
2017-10-02 07:13:41 -0400

    deliverables

---
+++ del2/script.js
@@ -0,0 +1,12 @@
+var number = prompt('Enter a number');
+for (i = 0; i > 0; i++) {
+    console.log(number + i);
+}
+
+var play = prompt('Do you want to play?');
+
+
+var name = prompt('Would you like to print your name?')
+while (name = 'yes') {
+  console.log('Hello my name is Adam' ++!);
+}
